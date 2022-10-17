73c84c636c68d072e919da3a90eaeba9f5b89fa3
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-10-17 22:18:07 +0530

    Task1

---
+++ samplem.py
@@ -0,0 +1,135 @@
+from itertools import product
+from flask import Flask, request, jsonify
+from flask_mongoengine import MongoEngine
+import json
+from healthcheck import HealthCheck
+app = Flask(__name__)
+health = HealthCheck()
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'Student',
+    'host': 'localhost',
+    'port': 27017
+}
+
+db = MongoEngine()
+db.init_app(app)
+
+class dpr(db.Document):
+    name = db.StringField()
+    age = db.IntField()
+    sub = db.StringField()
+    def to_json(self):
+        return {"name": self.name,
+                "age": self.age,
+                "sub" : self.sub}
+
+@app.route("/")
+def root_path():
+    return("sita")
+
+
+@app.route('/user/', methods=['GET'])
+def get_user():
+    contact = dpr.objects()
+    if not contact:
+        return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(contact)
+
+
+
+@app.route('/user/', methods=['POST'])
+def add_user():
+    record = json.loads(request.data)
+    contact = dpr(name=record['name'],
+                age=record['age'],
+                sub=record['sub'])
+    contact.save()
+    return jsonify(contact)
+
+
+@app.route('/user/<id>', methods=['PUT'])
+def Update_user(id):
+    record = json.loads(request.data)
+    contact = dpr.objects.get_or_404(id=id)
+    if not contact:
+        return jsonify({'error': 'data not found'})
+    else:
+        contact.update(name=record['name'],
+                    age=record['age'],
+                    sub=record["sub"])
+    return jsonify(contact)
+
+@app.route('/user/<id>', methods=['DELETE'])
+def delete_user(id):
+    contact = dpr.objects(id=id)
+    if not contact:
+        return jsonify({'error': 'data not found'})
+    else:
+        contact.delete()
+    return jsonify(contact)
+
+
+class car(db.Document):
+    name = db.StringField()
+    model = db.StringField()
+    price = db.StringField()
+    def to_json(self):
+        return {"name": self.name,
+                "model": self.model,
+                "price" : self.price}
+
+
+
+@app.route('/model/', methods=['GET'])
+def get_model():
+    product = car.objects()
+    if not product:
+        return jsonify({'error': 'data not found'})
+    else:
+        return jsonify(product)
+
+
+
+@app.route('/model/', methods=['POST'])
+def add_model():
+    record = json.loads(request.data)
+    product = car(name=record['name'],
+                model=record['model'],
+                price=record['price'])
+    product.save()
+    return jsonify(product)
+
+
+
+@app.route('/model/<id>', methods=['PUT'])
+def Update_model(id):
+    record = json.loads(request.data)
+    product = car.objects.get_or_404(id=id)
+    if not product:
+        return jsonify({'error': 'data not found'})
+    else:
+        product.update(name=record['name'],
+                    model=record['model'],
+                    price=record['price'])
+        # product.save()
+    return jsonify(product)
+
+
+@app.route('/model/<id>', methods=['DELETE'])
+def delete_model(id):
+    product = car.objects(id=id)
+    if not product:
+        return jsonify({'error': 'data not found'})
+    else:
+        product.delete()
+    return jsonify(product)
+
+
+
+
+app.add_url_rule('/healthcheck', 'healthcheck', view_func=lambda: health.run())
+
+if __name__ == "__main__":
+    app.run(debug=True)
